fea7faff0b4e8b3dd5a0909cc93ef2fe5bf7dc14
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-01-23 13:29:53 +0100

    feat: dinamic print of navbar menu

---
+++ data/navbarMenu.js
@@ -0,0 +1,7 @@
+const navItems = [
+  { id: 1, name: "Home", url: "/" },
+  { id: 2, name: "Chi Siamo", url: "/chi-siamo" },
+  { id: 3, name: "Prodotti", url: "/prodotti" }
+];
+
+export default navItems;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
+import navbarMenu from "../../data/navbarMenu";
 
 const NavBar = () => {
+  const { id, name, url } = navbarMenu;
   return (
     <nav>
       <ul className="nav">
-        <li className="nav-item">
+        {/* <li className="nav-item">
           <NavLink to="/">HOME</NavLink>
         </li>
         <li className="nav-item">
@@ -12,7 +14,12 @@ const NavBar = () => {
         </li>
         <li className="nav-item">
           <NavLink to="/prodotti">PRODOTTI</NavLink>
-        </li>
+        </li> */}
+        {navbarMenu.map((item) => (
+          <li key={item.id} className="nav-item">
+            <NavLink to={item.url}>{item.name}</NavLink>
+          </li>
+        ))}
       </ul>
     </nav>
   );

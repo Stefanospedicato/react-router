5576580a50f5a36bd3c30a6c3b9b8128b5fc81f5
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-01-23 13:11:14 +0100

    feat: add DefaultLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import Homepage from "./pages/Homepage";
+import AboutUsPage from "./pages/AboutUsPage";
+import ProductsPage from "./pages/ProductsPage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route Component={DefaultLayout}>
+          <Route />
+          <Route />
+          <Route />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/layouts/DefaultLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+const DefaultLayout = () => {
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>{Outlet}</main>
+    </>
+  );
+};
+
+export default DefaultLayout;

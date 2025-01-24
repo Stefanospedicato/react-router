3bfbd9226387e26024bb7e441fa80e008c69c427
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-01-24 11:55:31 +0100

    feat: add buttons properties in postDetail

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,5 +1,40 @@
 const AddPost = () => {
-  return <div>AddPost</div>;
+  // return (
+  //   <>
+  //     <div className="container my-5">
+  //       <form action="#" onSubmit={handlerSubmit}>
+  //         <div className="input-group">
+  //           <input
+  //             type="text"
+  //             className="form-control"
+  //             placeholder="Scrivi titolo da aggiungere..."
+  //             value={newTask.text}
+  //             onChange={handlerNewTask}
+  //           />
+  //           <button className="btn btn-outline-secondary" type="submit">
+  //             Aggiungi
+  //           </button>
+  //         </div>
+  //       </form>
+  //     </div>
+  //     <div className="container">
+  //       <ul className="list-group">
+  //         {myList.map((task) => (
+  //           <li
+  //             key={task.id}
+  //             className="list-group-item  d-flex justify-content-between"
+  //           >
+  //             {task.text}
+  //             <i
+  //               className="fa-solid fa-trash"
+  //               onClick={() => handlerRemove(task.id)}
+  //             ></i>
+  //           </li>
+  //         ))}
+  //       </ul>
+  //     </div>
+  //   </>
+  // );
 };
 
 export default AddPost;
